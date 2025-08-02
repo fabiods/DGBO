fb7d602ca14d226375bff3e0a5a32f0bce53afd4
Fabio Della Sala <fabio.dellasala@gmail.com>
2025-08-02 22:20:02 +0200

    Update boun2gmf.sh

--- boun2gmf.sh
+++ boun2gmf.sh
@@ -20,5 +20,6 @@ awk -v fmt=$GMF   '{printf fmt"\n",$1}' bounds.dat > tttmin
 
 awk -F E  '{print "1E"$2}' tttmin > ordg
 awk -v d=$dec '{print $1*d}' ordg > ordgdec
+awk -v d=1 '{print $1*d}' ordg > ordgdec0
 
 paste ordg ttt | awk -v d=$dec '{print $2/$1/d,$3/$1/d}'
