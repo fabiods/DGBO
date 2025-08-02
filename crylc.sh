5b071b03a0c4ac5e5a9bf01b6e0b102a92504b66
Fabio Della Sala <fabio.dellasala@gmail.com>
2025-08-02 13:31:08 +0200

    Update crylc.sh

--- crylc.sh
+++ crylc.sh
@@ -2,5 +2,17 @@
 name=$1
 
 awk 'f;/END/{f=1}' $name | sed '/END/q' | head -n -2 > tmp_basis
-bse convert-basis  --in-fmt  crystal --out-fmt jaguar tmp_basis tmp_basis_jag
-~/DGBO/readlc.x < tmp_basis_jag
+
+ns=`grep "*" tmp_basis | wc -l | awk '{print $1}'`
+#echo $ns
+
+nn=`head -n 1 tmp_basis | awk '{print $1}'`
+#echo $nn
+
+nb=`head -n 1 tmp_basis | awk -v k=$ns '{print $2-k}'`
+echo $nn $nb >tmp_basisx
+tail -n+2 tmp_basis > tmp_basisz
+sed -e '/*/{N;d;}' tmp_basisz >> tmp_basisx
+#bse convert-basis  --in-fmt  crystal --out-fmt jaguar tmp_basisx tmp_basis_jag
+~/DGBO/readlc2.x < tmp_basisx
+rm tmp_basisz tmp_basis
