48d437530199a46d3f6ad971b49e2f9b60f5ec45
Fabio Della Sala <fabio.dellasala@gmail.com>
2025-08-02 05:55:18 +0200

    Create ivvi.py

---
+++ ivvi.py
@@ -0,0 +1,49 @@
+def i2v(vali,scal,d):
+#       print("i2v(%d %f %d)" %(vali,scal,d))
+       shift=10**(d-1)  # 1 10 100
+       tens=10*shift    # 10 100 1000
+       scals=scal/shift
+#    if digits == 1:
+       if vali <   tens:
+          ttt= vali*scals
+       elif vali >=19*shift:
+#           print("ERROR i2v",vali,scal,d)
+           quit()
+       else: # i0*shif < []  < 19*shift
+#           print("i2v.vali%",(vali%tens))    
+           ttt= ( shift+(vali%tens) ) *scals*10
+#       print("i2v ->",ttt)    
+       return ttt
+#    elif digits ==2:
+#        if vali <= 100:
+#            return vali*scal
+#        else:
+#            return (int(vali/100)*100 +(vali%100)*10) *scal
+
+def v2i(val,scal,d):
+       shift=10**(d-1)   # 1 10 100
+       tens=10*shift     # 10 100 1000
+       scals=scal/shift  # E-3 E-4 E-5 
+       vs=round(val/(scals))
+#       print("v2i.vs",vs)
+#    if digits == 1:
+       if vs <=tens:
+            return vs
+#        elif vs>=95:
+#            print("ERROR v2i",val,scal,digits,vs)
+#            quit()
+       else:
+# val=1.1E-2 , scal E-3
+# scals=E-4
+# vs=110
+# vss=11
+              
+            vss=round(val/(scals*10)) # val/ E-2 E-3 E-4
+#            print("v2i.vss" ,vss)
+            return 10*shift+(vss-shift)
+#    elif digits ==2:
+#        if vs<100:
+#            return vs*shift
+#        else:
+#            vss=round(val/(scals*10))
+#            return 100+(vss-1)
