b40918d42c3187d4fa08013e3d39e3d48b115f26
Fabio Della Sala <fabio.dellasala@gmail.com>
2025-08-04 07:40:37 +0200

    Update ivn.py

--- ivn.py
+++ ivn.py
@@ -7,10 +7,17 @@ from ivvi import i2v,v2i
 a=float(sys.argv[1])
 scal=float(sys.argv[2])
 dig=int(sys.argv[3])
+shift=10**(dig-1)
 
 #print(a,scal,dig)
 ia=v2i(a,scal,dig)
-ia=ia+1
+
+if ia < 19*shift-1 :
+ ia=ia+1
+else :
+ scal=scal*10
+ ia=10*shift
+    
 an=i2v(ia,scal,dig)
 if dig==1:
     print("%2.0E" %(an))
