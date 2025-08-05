4e3535afdeba6c8bdefccaae70c09f1d9cfb3ce6
Fabio Della Sala <fabio.dellasala@gmail.com>
2025-08-05 15:39:51 +0200

    Update ivvi.py

--- ivvi.py
+++ ivvi.py
@@ -25,12 +25,13 @@ def v2i(val,scal,d):
        tens=10*shift     # 10 100 1000
        scals=scal/shift  # E-3 E-4 E-5 
        vs=round(val/(scals))
+       imaxvs=19*shift-1  
 #      print("v2i.vs",vs)
        if vs <=tens:
             return vs
-       elif vs>=95:
+       elif vs>=ivsmax:
             print("ERROR in v2i()",val,scal,d,":",vs,"Cannot be coded")
-            print(val," is outside the margins",i2v(1,scal,d),",",i2v(19*shift-1,scal,d))
+            print(val," is outside the margins",i2v(1,scal,d),",",i2v(ivsmax,scal,d))
             quit()
        else:
 # val=1.1E-2 , scal E-3
