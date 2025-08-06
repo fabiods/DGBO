149ae6c70544cbf640806e6543fcf95ec181894c
Fabio Della Sala <fabio.dellasala@gmail.com>
2025-08-06 08:32:36 +0200

    Update opt5zoo.py

--- opt5zoo.py
+++ opt5zoo.py
@@ -425,7 +425,7 @@ while bondsok == False or fbondsok == False:
 #    parameter = Parameter(budget=budget,init_samples=[x1],seed=10,intermediate_result=True,intermediate_freq=1)
     parameter =      Parameter(budget=budget,init_samples=[gx0],exploration_rate = 0.5,seed=10,precision=1.0E-7,stopping_criterion=StoppingCriterion(),intermediate_result=False,intermediate_freq=100)
     print("trainsize",parameter.get_train_size())
-    if nn<=8:
+    if nn<=7:
        parameter.set_train_size(20)
        parameter.set_positive_size(2)
        print("trainsize",parameter.get_train_size())
