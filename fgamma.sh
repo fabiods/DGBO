f22d6958299917e1a39beb9479c999ae05929f40
Fabio Della Sala <fabio.dellasala@gmail.com>
2025-08-15 07:07:32 +0200

    Update fgamma.sh

--- fgamma.sh
+++ fgamma.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-minr=`sort -k 3 -g notconv.dat  | grep -v NA | tail -n 1 | awk '{print $3}'`
-eneatminr=`sort -k 3 -g notconv.dat  | grep -v NA | tail -n 1 | awk '{print $6}'` 
+minr=`sort -k 3 -g -r notconv.dat  | grep -v NA | tail -n 1 | awk '{print $3}'`
+eneatminr=`sort -k 3 -g -r notconv.dat  | grep -v NA | tail -n 1 | awk '{print $6}'` 
 echo $minr $eneatminr
 
-mine=`sort -k 6 -g notconv.dat  | grep -v NA | tail -n 1 | awk '{print $6}'`  
-rmaxatmine=`sort -k 6 -g notconv.dat  | grep -v NA | tail -n 1 | awk '{print $3}'`
+mine=`sort -k 6 -g -r notconv.dat  | grep -v NA | tail -n 1 | awk '{print $6}'`  
+rmaxatmine=`sort -k 6 -g -r notconv.dat  | grep -v NA | tail -n 1 | awk '{print $3}'`
 echo $rmaxatmine $mine
 ediff=`echo $mine $eneatminr | awk '{print ($1-$2)/2}'`
 echo $ediff
