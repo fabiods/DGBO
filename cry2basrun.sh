53fc842e6fc4ba4cbd5170980941ca6a86b1f309
Fabio Della Sala <fabio.dellasala@gmail.com>
2025-08-21 16:57:34 +0200

    Update cry2basrun.sh

--- cry2basrun.sh
+++ cry2basrun.sh
@@ -108,7 +108,7 @@ for ((k = 0 ; k <= $num ; k++ )); do
     myexpa[$k]=`echo ${myexp[k]} ${myexp[k+1]} | awk '{ print sqrt($2*$1)}' `
     echo $k ${myexpa[k]}
 done
-if [ ${GMF:-"undef" == "undef" ]; then
+if [ ${GMF:-"undef"} == "undef" ]; then
    echo "GMF not set in the environment" 
    GMF="%5.3E"
 else
