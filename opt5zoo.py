83b310c01e8aa24097c8dc27f5d3b1ce7d9ff168
Fabio Della Sala <fabio.dellasala@gmail.com>
2025-08-21 07:30:44 +0200

    Update opt5zoo.py

--- opt5zoo.py
+++ opt5zoo.py
@@ -510,7 +510,7 @@ while bondsok == False or fbondsok == False:
         print(ackley(solution))   
         ggg=subprocess.run("cat basrunsed.dat", shell=True, stdout=subprocess.PIPE)
         print(ggg.stdout.decode('UTF-8'))
-        ggg=subprocess.run("~/DGBO/basderfol.sh", shell=True)
+        ggg=subprocess.run("~/DGBO/basderfol.sh 50", shell=True)
         menergylast=np.loadtxt('basderfol.energy', dtype='float', usecols=(1))
         print("energylast",menergylast)
         mxarr=np.loadtxt('basrunsed.dat', dtype='float', usecols=(1))
