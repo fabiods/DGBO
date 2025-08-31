1bd914ee44d256deb391d1670f4724e21098d0f7
Fabio Della Sala <fabio.dellasala@gmail.com>
2025-08-31 10:23:02 +0200

    Update opt5zoo.py

--- opt5zoo.py
+++ opt5zoo.py
@@ -98,7 +98,6 @@ class Scaling:
                 upb[i]=i2v(upbi[i], self.get(i)  ,dig)
 #                print("upbi",upbi[i],"upb",upb[
                 if abs(upb[i]) > bmax[i]:
-                    
                     bmaxint=v2i( bmax[i] ,self.get(i) ,dig)
                     upb[i]= -i2v(bmaxint,self.get(i) ,dig)
                     upbi[i]=bmaxint
