5637e4f9d4d30019c7659072d32714dc0406bcff
Fabio Della Sala <fabio.dellasala@gmail.com>
2025-09-03 09:18:21 +0200

    Update xdiff.f

--- xdiff.f
+++ xdiff.f
@@ -37,7 +37,9 @@
       do i=1,n
          
          do k=1,nc
-         if (x1(i,k).ge.1.d0) then
+         if (x1(i,k).ge.10.d0) then
+            ok(k)=10
+         else if (x1(i,k).ge.1.d0) then
             ox(k)=1
          else if (x1(i,k).ge.0.1d0) then
             ox(k)=0.1d0
