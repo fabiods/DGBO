e12e55b0023d6a86619a68cc9150bd9bd6319f49
Fabio Della Sala <fabio.dellasala@gmail.com>
2025-11-23 08:16:10 +0100

    Create doincmax.sh

---
+++ doincmax.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+grep incmax td.dat |sort -k 2 -g -r > list.incmax
+ttol=0.0000001
+while read -r line; do
+    lone=`echo $line |  awk '{print $2}'`
+    ltwo=`echo $line |  awk '{print $3}'`
+
+    chdetot=`echo "sqrt($lone*$lone) <= $ttol" | bc -l`
+    # this is zero, because it is not converegd !
+    chdetotwo=`echo "sqrt($ltwo*$ltwo) <= 10*$ttol" | bc -l`  
+    echo $lone $chdetot $ltwo $chdetotwo
+    if [ "$chdetot" == "1" ] && [ "$chdetotwo" == "1" ]; then
+	file=`echo $line | awk '{print $1}'`
+	echo $file
+	mv $file reallyconv
+    fi
+done < list.incmax  
