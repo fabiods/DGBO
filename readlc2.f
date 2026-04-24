d31696a77c513c553639950b90cf844c69e51557
Fabio Della Sala <fabio.dellasala@gmail.com>
2026-04-24 08:12:15 +0200

    Add new variables and enhance input reading logic

--- readlc2.f
+++ readlc2.f
@@ -1,8 +1,8 @@
       program readlc
       implicit none
       character*2 ch
-      integer k,ks,kp,kd,kf,z,num,izero,ich,iocc
-      double precision ess(50),epp(50),edd(50)  ,iuno
+      integer k,ks,kp,kd,kf,kg,z,num,izero,ich,iocc
+      double precision ess(50),epp(50),edd(50),eff(5),edd(50)  ,iuno
 # read directly input.d12
       read(*,*)
 #      read(*,*)
@@ -10,6 +10,7 @@
       kp=0
       kd=0
       kf=0
+      kg=0
  88   continue
 # 0 0  6  2. 1.
 # 0 2  1  0. 1. *
@@ -19,6 +20,7 @@
       if (ich.eq.2) ch='P'
       if (ich.eq.3) ch='D'
       if (ich.eq.4) ch='F'
+      if (ich.eq.5) ch='G'
       if (ch.eq.'S') then
        do k=1,num
         ks=ks+1
@@ -40,6 +42,20 @@
         write(*,*) edd(kd)
        end do
       end if
+      if (ch.eq.'F') then
+       do k=1,num
+        kf=kf+1
+        read(*,*) eff(kf)
+        write(*,*) eff(kf)
+       end do
+      end if 
+      if (ch.eq.'G') then
+       do k=1,num
+        kd=kd+1
+        read(*,*) egg(kg)
+        write(*,*) egg(kg)
+       end do
+      end if
       goto 88
 89    write(*,*) 'done'
       if (ks.gt.0) then
@@ -51,4 +67,10 @@
       if (kd.gt.0) then
          write(*,*) 'D',edd(kd)
       endif   
+     if (kf.gt.0) then
+         write(*,*) 'F',eff(kd)
+      endif
+     if (kg.gt.0) then
+         write(*,*) 'G',egg(kd)
+      endif
       end program
